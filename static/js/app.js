0a0bdf498c85fd48502b16ca98822fb075896b55
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-17 13:21:45 -0800

    added js logic to handle form submission

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,24 @@
-$guessInput = $('#guess')[0]
+$(function() {
+    const $guessInput = $('#guess')[0] // user guess input field
+    
+    const makeAjaxCall = async (guess) => {
+        try {
+            res = await axios.post('/guess', { guess }) // submit guess to backend
+            console.log(res.data.result)
+        } catch (error) {
+            throw new Error(`Somthing went wrong => message: ${error}`)
+        }
+    }
+    
+    const handleUserGuess = (e) => {
+        e.preventDefault() // prevent browser from redirecting to a new page
+        guess = $guessInput.value // extract user guess
+        $guessInput.value = '' // reset input value
+    
+        makeAjaxCall(guess)
+    }
+    
+    $('.word-form').on('submit', handleUserGuess)
 
-guess = $guessInput.value
+})
+
