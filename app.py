6ce0b87a85ec03287e36e8d23b496139bc3a28af
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-17 13:20:55 -0800

    added route for incoming post request from user, receives and sends a JSON object. User will resceive the results if the guessed word is valid or not or does not exist

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, session, request, redirect, render_template
+from flask import Flask, session, request, redirect, render_template, jsonify
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,13 +9,12 @@ app.config['SECRET_KEY'] = 'SecretPhrase'
 
 boggle_game = Boggle()
 
-
-@app.before_request
-def print_cookies():
-    print('****************')
-    print('COOKIES', session.get('game_board'))
-    print('COOKIES', session.get('played'))
-    print('****************')
+# @app.before_request
+# def print_cookies():
+#     print('****************')
+#     print('COOKIES', session.get('game_board'))
+#     print('COOKIES', session.get('played'))
+#     print('****************')
 
 
 @app.route("/")
@@ -24,3 +23,17 @@ def home():
     session['game_board'] = boggle_game.make_board()
     session['played'] = session.get('played', 0) + 1
     return render_template("index.html", game_board=session['game_board'])
+
+
+@app.route("/guess", methods=["POST"])
+def word_guessed():
+    """ Checks guessed word if it is valid """
+
+    # get user guess
+    user_guess = request.json['guess']
+    # get board cookie from client
+    board = session['game_board']
+    # is the word valid
+    result = boggle_game.check_valid_word(board, user_guess)
+    # send json over to client with result of word submitted
+    return jsonify({"result": result})
