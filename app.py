c60e9eb49ec7814736191f210ee5f5a71f67ce61
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-18 07:32:35 -0800

    added score

--- app.py
+++ app.py
@@ -3,26 +3,27 @@ from flask import Flask, session, request, redirect, render_template, jsonify
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SecretPhrase'
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config["SECRET_KEY"] = "SecretPhrase"
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 # debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
 # @app.before_request
 # def print_cookies():
-#     print('****************')
-#     print('COOKIES', session.get('game_board'))
-#     print('COOKIES', session.get('played'))
-#     print('****************')
+#     print("****************")
+#     print("COOKIES", session.get("game_board"))
+#     print("COOKIES", session.get("played"))
+#     print("****************")
 
 
 @app.route("/")
 def home():
     """ Initialize game board and times played """
-    session['game_board'] = boggle_game.make_board()
-    session['played'] = session.get('played', 0) + 1
-    return render_template("index.html", game_board=session['game_board'])
+    session["game_board"] = boggle_game.make_board()
+    session["played"] = session.get("played", 0) + 1
+    session["score"] = 0
+    return render_template("index.html", game_board=session["game_board"])
 
 
 @app.route("/guess", methods=["POST"])
@@ -30,10 +31,14 @@ def word_guessed():
     """ Checks guessed word if it is valid """
 
     # get user guess
-    user_guess = request.json['guess']
+    user_guess = request.json["guess"]
     # get board cookie from client
-    board = session['game_board']
+    board = session["game_board"]
     # is the word valid
     result = boggle_game.check_valid_word(board, user_guess)
+    # ok, not-on-board, not-word
+    if result == "ok":
+        score = session["score"] + len(user_guess)
+        session["score"] = score
     # send json over to client with result of word submitted
-    return jsonify({"result": result})
+    return jsonify({"result": result, "score": score})
