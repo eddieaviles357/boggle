cba01a6171c3390c1ded0dc0d40bf33632ca1b0a
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-20 20:36:42 -0800

    added gameOver ajax call that updates ui elements and make a call to the server and updates session cookies

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,87 @@
 $(function() {
     const $guessInput = $('#guess')
     const $score = $('.score')
+    const $highScore = $('#high-score')
     const $result = $('.result')
-    // Makes an ajax call to our backend and responds with JSON object
-    const makeAjaxCall = async guess => {
-        try {
-            const {data} = await axios.post('/guess', { guess })
-            return data.result
-        } catch (error) {
-            throw new Error(`Somthing went wrong => message: ${error}`)
-        }
-    }
-    // populate results text content
-    const showResults = (rslts, wordGuessed) => {
-        score = +($score.text()) // get score and cast to int
-        $result.text(rslts)
+    const $timer = $('.timer > span')
+    const $wrapper = $('.wrapper')
+    const $gameOver = $('.game-over')
+    const $timesPlayed = $('#times-played')
+    // const $highScore = $('#high-score')
 
-        if (rslts == 'ok'){
-            score = score + wordGuessed.length
+    const getScore = () => +($score.text())
+    const getTime = () => +($timer.text())
+    const updateElementText = ( $element, val ) => { $element.text(val) }
+    const hideElement = $element => { $element.hide() }
+    const isGameOver = () => getTime() <= 0
+    const updateTimer = async () => {
+        let time = getTime()
+        time -= 1
+        $timer.text(time)
+        // stop timer when 0 is hit and end game
+        if (isGameOver()) { 
+            clearInterval(intervalID)
+            let {score, gamesPlayed} = await gameOver()
         }
-        $score.text(score)
     }
     
+    // start inverval and store id
+    let intervalID = setInterval(updateTimer,1000)
+    // get current score casted to int
+
+
+    // Makes an ajax call to our backend and responds with JSON object
+    const guessAjaxCall = async guess => {
+        try {
+            const {data} = await axios.post('/guess', { guess })
+            return data
+        } catch (error) {
+            throw new Error(`Something went wrong => message: ${error}`)
+        }
+    }
+
+    const gameOver = async () => {
+        try {
+            let score = getScore()
+            const {data} = await axios.post('/game-over', {score})
+            hideElement($wrapper)
+            hideElement($gameOver)
+            updateElementText($highScore, data.score)
+            updateElementText($timesPlayed, data.gamesPlayed)
+            return data
+        } catch (error) {
+            throw new Error(`Something went wrong => message: ${error}`)
+        }
+    }
+
+
+    // populate results text content
+    const showResults = ({result, isDuplicate}, wordGuessed) => {
+        let score = getScore()
+        $result.text(result)
+
+        if (result == 'ok') { score = score + wordGuessed.length }
+        if (isDuplicate) {
+            $result.text("Word already used")
+        }
+        updateElementText($score, score)
+    }
+    
+
     // Handles form submission
     const handleUserGuess = async e => {
         e.preventDefault() // prevent browser from redirecting to a new page
-        guess = $guessInput.val() // extract user guess
+        let guess = $guessInput.val() // extract user guess
 
         if (guess.length >= 3) {
-            response = await makeAjaxCall(guess)
+            response = await guessAjaxCall(guess)
             showResults(response, guess)
             $guessInput.val('') // reset input value
         } else {
             $result.text('Must be 3 or more letters')
         }
-
     }
     
     $('.word-form').on('submit', handleUserGuess)
-
-    
 })
 
